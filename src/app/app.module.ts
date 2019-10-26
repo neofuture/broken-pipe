4eb3bc827600470a268f042b84587e4c88c4212d
Carl Fearby <carlfearby@me.com>
2019-10-26 13:55:42 +0100

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WindowComponent } from './components/window/window.component';
+import { DesktopComponent } from './components/desktop/desktop.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { TitlebarComponent } from './components/titlebar/titlebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WindowComponent,
+    DesktopComponent,
+    ToolbarComponent,
+    TitlebarComponent
   ],
   imports: [
     BrowserModule
