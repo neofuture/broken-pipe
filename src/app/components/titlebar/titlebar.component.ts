a3f9cc714964654540fec4ff568de07e9a08f379
Carl Fearby <carlfearby@me.com>
2019-10-31 21:49:59 +0000

    random icons on windows

--- src/app/components/titlebar/titlebar.component.ts
+++ src/app/components/titlebar/titlebar.component.ts
@@ -12,16 +12,40 @@ export class TitleBarComponent implements OnInit {
   hasTab = true;
   resizable = true;
   icon = 'locationPin';
+  iconArray = [
+    'alarm', 'bin', 'catalogue_over', 'catalogue', 'catalogues_over',
+    'catalogues', 'clipboard', 'clock', 'cloud', 'cog_over', 'cog',
+    'company_over', 'company', 'computer', 'contacts_over', 'contacts',
+    'content_area', 'creditcard', 'dashboard_over', 'dashboard', 'diary_over',
+    'diary', 'downArrow', 'download', 'duplicate_page', 'email',
+    'exclamation_mark_circle', 'external_link', 'eye', 'fat_close', 'fat_tick',
+    'folder', 'forms', 'formsResponses', 'framework', 'funnel_over', 'funnel',
+    'ghost_over', 'ghost', 'globe', 'home_over', 'home', 'leftArrow',
+    'leftArrowStart', 'link', 'locationPin', 'lock_closed', 'lock_open',
+    'media', 'menu_over', 'menu', 'messages_over', 'messages', 'minus',
+    'move', 'orders_over', 'orders', 'page_add', 'page_settings', 'page_tags',
+    'pages', 'pencil', 'phone_over', 'phone', 'piechart_over', 'piechart',
+    'playbutton', 'plus', 'print_over', 'print', 'question_mark_circle',
+    'question_mark', 'quotations_over', 'quotations', 'reminder_over',
+    'reminder', 'reorder', 'rightArrow', 'rightArrowEnd', 'save', 'search_over',
+    'search', 'styles', 'tasks_over', 'tasks', 'templates', 'tree_corner',
+    'tree_line_horizontal', 'tree_line', 'tree_t', 'upArrow', 'users_over',
+    'users', 'world', 'oceanworks', 'minimise', 'restore', 'maximise', 'close'];
+
 
   constructor(private windowService: WindowService) {
   }
 
   ngOnInit() {
-    this.windowService.new('diary', true, 'No Tab', false, true);
-    this.windowService.new('diary', true, 'Not Resizable', true, false);
-    this.windowService.new('diary', false, 'No Title Bar', true, true);
-    this.windowService.new('diary', true, 'Test Window 4', true, true);
-    this.windowService.new('diary', true, 'Test Window 5', true, true);
+    this.windowService.new(this.getIcon(), true, 'No Tab', false, true);
+    this.windowService.new(this.getIcon(), true, 'Not Resizable', true, false);
+    this.windowService.new(this.getIcon(), false, 'No Title Bar', true, true);
+    this.windowService.new(this.getIcon(), true, 'Test Window 4', true, true);
+    this.windowService.new(this.getIcon(), true, 'Test Window 5', true, true);
+  }
+
+  getIcon() {
+    return this.iconArray[Math.floor(Math.random() * this.iconArray.length)];
   }
 
   toggleBlocky() {
